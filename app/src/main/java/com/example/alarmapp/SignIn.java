06ad1feb2b57b08eaaf5745c01bed700fef3ac7e
KanazawaYui <osaka.hakuba.18@gmail.com>
2020-09-03 23:09:24 +0900

    ホーム画面までの大まかなUI配置と、とりあえずサインインからの遷移

--- app/src/main/java/com/example/alarmapp/SignIn.java
+++ app/src/main/java/com/example/alarmapp/SignIn.java
@@ -2,7 +2,10 @@ package com.example.alarmapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
 
@@ -11,15 +14,25 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin); //ここを対応するxmlファイルに変更
+
+
+        //ID, パスワードをデータベースに渡す
+
+
+        //SignIn_buttonが押された時用の画面遷移関数
+        //データベースへ渡す関数の実行
+        //Title.javaを呼び出す？(画面遷移)
+        Button signIn_button = findViewById(R.id.signIn_button);
+        signIn_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_signIn = new Intent(getApplication(),Title.class);
+                startActivity(intent_signIn);
+            }
+        });
     }
 
-    //ID, パスワードをデータベースに渡す
 
-
-
-    //SignIn_buttonが押された時用の画面遷移関数
-    //データベースへ渡す関数の実行
-    //Title.javaを呼び出す？(画面遷移)
 
 
 
